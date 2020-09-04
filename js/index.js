1ef0359228add7fafee4b0c1c1b3c9f816ff71fd
jixindong <2010342518@qq.com>
2020-09-04 11:35:57 +0800

    add

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 let homeFunc = ((win) => {
-    // 改变导航栏背景色
-    function handleHdBgc() {
+    // 事件绑定
+    function eventBind() {
+        // 改变导航栏背景色
         let hd = document.querySelector('#hd');
-        let bannerHeight = document.querySelector('#banner-carousel').offSetHeight || 600;
+        let bannerHeight = document.querySelector('#banner-carousel').offSetHeight || 800;
         win.onscroll = () => {
             let scrollTop = document.documentElement.scrollTop || win.pageYOffset || document.body.scrollTop;
             if (scrollTop >= bannerHeight) {
@@ -10,31 +11,42 @@ let homeFunc = ((win) => {
             } else {
                 hd.classList.remove('hd-theme-color');
             }
-        }
-    };
-    // 带货tab切换
-    function sellGoodTab() {
-        let sellGoodTabs = document.querySelectorAll('#sellGoodTabs .item');
-        let sellGoddItems = document.querySelectorAll('#sellGoodItems .item');
-        for (let i = 0; i < sellGoodTabs.length; i++) {
-            sellGoodTabs[i].onclick = () => {
-                for (let j = 0; j < sellGoddItems.length; j++) {
-                    sellGoodTabs[j].classList.remove('active');
-                    sellGoddItems[j].classList.remove('active');
+        };
+
+        // 显示、隐藏导航栏
+        $('#dropdown').click(() => {
+            $('#hd').addClass('hd-theme-color');
+            $('#hide-nav').slideToggle();
+        });
+
+        // 服务体系鼠标悬浮事件
+        $('#serviceSys .item').each(function () {
+            $(this).hover(() => {
+                let textHeight = $(this).height() - $(this).find('.title').height();
+                $(this).find('.text').css('height', textHeight).slideToggle();
+            });
+        });
+
+        // 公司动态tab切换
+        let newsTabs = document.querySelectorAll('#newsTabs .item');
+        let newsList = document.querySelectorAll('#newsList .item');
+        for (let i = 0; i < newsTabs.length; i++) {
+            newsTabs[i].onclick = () => {
+                for (let j = 0; j < newsList.length; j++) {
+                    newsTabs[j].classList.remove('active');
+                    newsList[j].classList.remove('active');
                 };
-                sellGoodTabs[i].classList.add('active');
-                sellGoddItems[i].classList.add('active');
+                newsTabs[i].classList.add('active');
+                newsList[i].classList.add('active');
             };
         };
     };
 
     return {
-        handleHdBgc,
-        sellGoodTab
+        eventBind
     };
 })(window);
 
-$(function () {
-    homeFunc.handleHdBgc(); // 改变导航栏背景色
-    homeFunc.sellGoodTab(); // 带货tab切换
+$(() => {
+    homeFunc.eventBind(); // 事件绑定
 });
